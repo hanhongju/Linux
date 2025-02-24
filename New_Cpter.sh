57db33a5793ddd92b34ada3dbd665c6d4c78a889
hanhongju <63781648+hanhongju@users.noreply.github.com>
2025-02-24 22:37:10 +0800

    Update New_Cpter.sh

--- New_Cpter.sh
+++ New_Cpter.sh
@@ -1,5 +1,14 @@
 apt update
-apt -y install brasero
+apt -y install brasero rhythmbox totem
+
+
+
+
+echo    '
+0 1 * * *     apt           -y          update
+0 2 * * *     apt           -y          full-upgrade
+0 3 * * *     apt           -y          autoremove
+'       |     crontab
 
 
 
