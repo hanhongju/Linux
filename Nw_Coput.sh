0b823038e9f4030b937a070f6cdc0b07cc00043b
hanhongju <63781648+hanhongju@users.noreply.github.com>
2025-02-24 22:48:02 +0800

    Update Nw_Coput.sh

--- Nw_Coput.sh
+++ Nw_Coput.sh
@@ -1,9 +1,10 @@
-apt     -y    install brasero rhythmbox vlc
+apt     -y    install brasero rhythmbox vlc net-tools
 echo    '
 0 1 * * *     apt           -y          update
 0 2 * * *     apt           -y          full-upgrade
 0 3 * * *     apt           -y          autoremove
 '       |     crontab
+netstat       -plnt
 
 
 
