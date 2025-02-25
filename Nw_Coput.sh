76a5f30f60a0f77cd1f7fc21d222d19872897b98
hanhongju <63781648+hanhongju@users.noreply.github.com>
2025-02-25 17:10:11 +0800

    Update Nw_Coput.sh

--- Nw_Coput.sh
+++ Nw_Coput.sh
@@ -1,3 +1,13 @@
+
+echo '
+Types: deb
+URIs: https://mirrors.tuna.tsinghua.edu.cn/ubuntu/
+Suites: noble noble-updates noble-security
+Components: main restricted universe multiverse
+Signed-By: /usr/share/keyrings/ubuntu-archive-keyring.gpg
+' > /etc/apt/sources.list.d/ubuntu.sources
+apt update
+
 apt     -y    install brasero rhythmbox vlc net-tools libreoffice
 echo    '
 0 1 * * *     apt           -y          update
