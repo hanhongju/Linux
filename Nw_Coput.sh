266deb1c5e136b416c43911ff9aaa96e7a08d3f4
hanhongju <63781648+hanhongju@users.noreply.github.com>
2025-02-25 17:37:33 +0800

    Update Nw_Coput.sh

--- Nw_Coput.sh
+++ Nw_Coput.sh
@@ -22,7 +22,7 @@ URIs: http://mirrors.tuna.tsinghua.edu.cn/ubuntu/
 Suites: noble noble-updates noble-security
 Components: main restricted universe multiverse
 Signed-By: /usr/share/keyrings/ubuntu-archive-keyring.gpg
-' > /etc/apt/sources.list.d/ubuntu.sources
+'       >     /etc/apt/sources.list.d/ubuntu.sources
 
 apt update
 
