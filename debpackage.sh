7b4dbd777156c6fd52e83409fa931b5312a9c6c9
hanhongju <63781648+hanhongju@users.noreply.github.com>
2025-02-27 01:01:20 +0800

    Update debpackage.sh

--- debpackage.sh
+++ debpackage.sh
@@ -1,15 +1,13 @@
-apt    -y       install  curl
-curl   --socks5-hostname    127.0.0.1:8000           \
-       --remote-name   --location  --continue-at -   \
-       https://github.com/2dust/v2rayN/releases/download/7.9.3/v2rayN-linux-64.deb
-dpkg   -i       v2rayN-linux-64.deb
+apt    -y   install  curl
+curl        --socks5-hostname    127.0.0.1:8000           \
+            --remote-name  --location  --continue-at -    \
+            https://github.com/2dust/v2rayN/releases/download/7.9.3/v2rayN-linux-64.deb
+dpkg   -i   v2rayN-linux-64.deb
 
 
-
-
-curl   --remote-name   --location  --continue-at -   \
-       https://dldir1v6.qq.com/weixin/Universal/Linux/WeChatLinux_x86_64.deb
-dpkg   -i       WeChatLinux_x86_64.deb
+curl        --remote-name   --location  --continue-at -   \
+            https://dldir1v6.qq.com/weixin/Universal/Linux/WeChatLinux_x86_64.deb
+dpkg   -i   WeChatLinux_x86_64.deb
 
 
 
