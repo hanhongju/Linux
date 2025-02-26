e50ae92828381a7cb617da1b836a167182423cfb
hanhongju <63781648+hanhongju@users.noreply.github.com>
2025-02-26 17:27:52 +0800

    Update v2rayN.sh

--- v2rayN.sh
+++ v2rayN.sh
@@ -1,8 +1,9 @@
-apt    -y   install  curl
-curl    -L  --socks5-hostname    127.0.0.1:8000  \
-        -O   https://github.com/2dust/v2rayN/releases/download/7.9.3/v2rayN-linux-64.deb
-
+apt    -y    install  curl
+curl   -L    --socks5-hostname    127.0.0.1:8000  \
+       -O    https://github.com/2dust/v2rayN/releases/download/7.9.3/v2rayN-linux-64.deb
 dpkg   -i    v2rayN-linux-64.deb
 
 
 
+
+# 安装v2rayN @ Ubuntu 24.04.2 LTS
