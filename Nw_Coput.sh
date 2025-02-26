ec047f410c732180d586ecb70adb3b981f74bacf
hanhongju <63781648+hanhongju@users.noreply.github.com>
2025-02-26 14:55:29 +0800

    Update Nw_Coput.sh

--- Nw_Coput.sh
+++ Nw_Coput.sh
@@ -29,7 +29,7 @@ apt update
 
 
 
-apt     -y    install brasero rhythmbox vlc net-tools libreoffice libreoffice-l10n-zh-cn libreoffice-help-zh-cn hashdeep p7zip-full
+apt     -y    install brasero rhythmbox vlc net-tools libreoffice libreoffice-l10n-zh-cn libreoffice-help-zh-cn hashdeep p7zip-full samba
 echo    '
 0 1 * * *     apt           -y          update
 0 2 * * *     apt           -y          full-upgrade
