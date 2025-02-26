20799f07a45aa67681b9d903f7c3714ff5848580
hanhongju <63781648+hanhongju@users.noreply.github.com>
2025-02-26 17:26:34 +0800

    Create v2rayN.sh

---
+++ v2rayN.sh
@@ -0,0 +1,8 @@
+apt    -y   install  curl
+curl    -L  --socks5-hostname    127.0.0.1:8000  \
+        -O   https://github.com/2dust/v2rayN/releases/download/7.9.3/v2rayN-linux-64.deb
+
+dpkg   -i    v2rayN-linux-64.deb
+
+
+
