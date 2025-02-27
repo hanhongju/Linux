8d987b8f1a321c1c932fd34833e1f6bc0029d5d3
hanhongju <63781648+hanhongju@users.noreply.github.com>
2025-02-27 10:02:34 +0800

    Update debpackage.sh

--- debpackage.sh
+++ debpackage.sh
@@ -5,9 +5,15 @@ curl        --socks5-hostname    127.0.0.1:8000           \
 dpkg   -i   v2rayN-linux-64.deb
 
 
+curl        --socks5-hostname    127.0.0.1:8000           \
+            --remote-name  --location  --continue-at -    \
+            https://github.com/balena-io/etcher/releases/download/v1.19.25/balena-etcher_1.19.25_amd64.deb
+apt    -y   install   ./balena-etcher_1.19.25_amd64.deb
+
+
 curl        --remote-name   --location  --continue-at -   \
             https://dldir1v6.qq.com/weixin/Universal/Linux/WeChatLinux_x86_64.deb
-apt         install     ./WeChatLinux_x86_64.deb
+apt    -y   install     ./WeChatLinux_x86_64.deb
 
 
 
