eddfdc3c2a0113b4d4690f98ebe629301ad7e8cb
hanhongju <63781648+hanhongju@users.noreply.github.com>
2025-02-28 23:23:13 +0800

    Update Nw_Coput.sh

--- Nw_Coput.sh
+++ Nw_Coput.sh
@@ -11,8 +11,8 @@ apt update
 
 
 dpkg    -a    --configure
-apt     -y    install wget curl brasero rhythmbox vlc net-tools libreoffice libreoffice-l10n-zh-cn libreoffice-help-zh-cn
-apt     -y    install hashdeep p7zip-full firefox gparted usb-creator-gtk samba qbittorrent cifs-utils nvidia-driver-libs:i386 libnvidia-gl-535:i386 steam
+apt     -y    install wget curl brasero rhythmbox vlc net-tools libreoffice libreoffice-l10n-zh-cn libreoffice-help-zh-cn libnvidia-gl-535:i386
+apt     -y    install hashdeep p7zip-full firefox gparted usb-creator-gtk samba qbittorrent cifs-utils steam
 snap          install firefox
 echo    '
 0 1 * * *     apt           -y          update
