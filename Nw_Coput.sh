89693ee3523de103935bc4655132587ebd07a3be
hanhongju <63781648+hanhongju@users.noreply.github.com>
2025-03-02 00:00:38 +0800

    Update Nw_Coput.sh

--- Nw_Coput.sh
+++ Nw_Coput.sh
@@ -14,7 +14,7 @@ apt update
 ubuntu-drivers autoinstall
 dpkg    -a     --configure
 apt     -y     install wget curl brasero rhythmbox smplayer net-tools libreoffice libreoffice-l10n-zh-cn libreoffice-help-zh-cn
-apt     -y     install hashdeep p7zip-full firefox gparted usb-creator-gtk samba qbittorrent cifs-utils steam thunderbird
+apt     -y     install hashdeep p7zip-full firefox gparted usb-creator-gtk samba qbittorrent cifs-utils steam thunderbird handbrake
 snap           install firefox
 apt     -y     autoremove
 echo    '
