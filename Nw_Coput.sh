f0a5161753e3d30d287f2718233dce108c0c7558
hanhongju <63781648+hanhongju@users.noreply.github.com>
2025-03-03 23:02:47 +0800

    Update Nw_Coput.sh

--- Nw_Coput.sh
+++ Nw_Coput.sh
@@ -4,6 +4,7 @@ Suites: noble noble-updates noble-security
 Components: main restricted universe multiverse
 Signed-By: /usr/share/keyrings/ubuntu-archive-keyring.gpg
 '       >     /etc/apt/sources.list.d/ubuntu.sources
+# Comprehensive R Archive Network mirror sources
 echo    'deb https://mirrors.tuna.tsinghua.edu.cn/CRAN/bin/linux/ubuntu noble-cran40/'     >    /etc/apt/sources.list.d/cran40.list
 wget    https://mirrors.tuna.tsinghua.edu.cn/CRAN/bin/linux/ubuntu/marutter_pubkey.asc    -O    /etc/apt/trusted.gpg.d/cran40.asc
 
