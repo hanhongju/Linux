d8fe6acbc318a638c934acc4d46fc8f54b93f019
hanhongju <63781648+hanhongju@users.noreply.github.com>
2025-03-03 11:08:09 +0800

    Update debpackage.sh

--- debpackage.sh
+++ debpackage.sh
@@ -7,7 +7,7 @@ curl        --socks5-hostname    127.0.0.1:8000           \
             --remote-name        https://dldir1v6.qq.com/weixin/Universal/Linux/WeChatLinux_x86_64.deb   \
             --remote-name        https://download1.rstudio.org/electron/jammy/amd64/rstudio-2024.12.1-563-amd64.deb
 dpkg   -i   v2rayN-linux-64.deb
-apt    -y   install             ./WeChatLinux_x86_64.deb    ./balena-etcher_1.19.25_amd64.deb
+apt    -y   install             ./WeChatLinux_x86_64.deb   ./balena-etcher_1.19.25_amd64.deb   ./rstudio-2024.12.1-563-amd64.deb
 
 
 
