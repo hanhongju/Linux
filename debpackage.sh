a4ab0b3a6ca9e6bafcf0b4df8d31dd9f6a7401ff
hanhongju <63781648+hanhongju@users.noreply.github.com>
2025-03-04 21:33:21 +0800

    Update debpackage.sh

--- debpackage.sh
+++ debpackage.sh
@@ -5,8 +5,9 @@ curl        --socks5-hostname    127.0.0.1:8000           \
             --remote-name        https://github.com/2dust/v2rayN/releases/download/7.9.3/v2rayN-linux-64.deb   \
             --remote-name        https://github.com/balena-io/etcher/releases/download/v1.19.25/balena-etcher_1.19.25_amd64.deb   \
             --remote-name        https://dldir1v6.qq.com/weixin/Universal/Linux/WeChatLinux_x86_64.deb   \
-            --remote-name        https://download1.rstudio.org/electron/jammy/amd64/rstudio-2024.12.1-563-amd64.deb
-apt    -y   install              ./v2rayN-linux-64.deb   ./WeChatLinux_x86_64.deb   ./balena-etcher_1.19.25_amd64.deb   ./rstudio-2024.12.1-563-amd64.deb
+            --remote-name        https://download1.rstudio.org/electron/jammy/amd64/rstudio-2024.12.1-563-amd64.deb    \
+            --remote-name        https://dl.hostbuf.com/finalshell3/finalshell_linux_x64.deb
+apt    -y   install              ./v2rayN-linux-64.deb   ./WeChatLinux_x86_64.deb   ./balena-etcher_1.19.25_amd64.deb   ./rstudio-2024.12.1-563-amd64.deb      ./finalshell_linux_x64.deb
 
 
 
