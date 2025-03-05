9690b9de160d513dde91bc6db3b5863ef656b26f
hanhongju <63781648+hanhongju@users.noreply.github.com>
2025-03-05 17:16:07 +0800

    Create share.sh

---
+++ share.sh
@@ -0,0 +1,8 @@
+sed            -i        "/vmhgfs/d"       /etc/fstab
+#  mount       .host:/  /mnt/  --types fuse.vmhgfs-fuse  --options allow_other
+echo          '.host:/  /mnt/  fuse.vmhgfs-fuse  allow_other  0  0'  >>  /etc/fstab
+
+
+
+
+# 开启VMware虚拟机共享文件夹
