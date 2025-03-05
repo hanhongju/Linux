d2c2c03fc8f4b4679b1e7dfb5c2f8b42b42020bc
hanhongju <63781648+hanhongju@users.noreply.github.com>
2025-03-05 14:15:34 +0800

    Update share.sh

--- share.sh
+++ share.sh
@@ -1,4 +1,4 @@
-mount .host:/ /mnt/ --types fuse.vmhgfs-fuse --options allow_other
+mount  .host:/  /mnt/  --types fuse.vmhgfs-fuse  --options allow_other
 
 mount.cifs -o user=test,password=123 //192.168.1.100/gostreet /mnt
 
