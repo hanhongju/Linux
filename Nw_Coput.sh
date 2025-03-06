d7cea0b5972fca16ae1222456ca90994ff7a68e3
hanhongju <63781648+hanhongju@users.noreply.github.com>
2025-03-06 12:14:31 +0800

    Update Nw_Coput.sh

--- Nw_Coput.sh
+++ Nw_Coput.sh
@@ -18,7 +18,7 @@ ubuntu-drivers autoinstall
 dpkg    -a     --configure
 apt     -y     install wget curl brasero rhythmbox smplayer net-tools libreoffice libreoffice-l10n-zh-cn libreoffice-help-zh-cn
 apt     -y     install hashdeep p7zip-full firefox gparted usb-creator-gtk samba qbittorrent cifs-utils steam thunderbird
-apt     -y     install handbrake libcurl4-openssl-dev software-properties-common dirmngr r-base r-base-dev libmagick++-dev cmake calibre
+apt     -y     install handbrake libcurl4-openssl-dev software-properties-common dirmngr r-base libmagick++-dev cmake calibre
 apt     -y     autoremove
 chmod   777    --recursive   /usr/lib/R/library/
 echo    '
