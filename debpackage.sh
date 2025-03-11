3b8b29bd84f29225ffc19afccfcbeabd215e2f87
hanhongju <63781648+hanhongju@users.noreply.github.com>
2025-03-11 17:12:01 +0800

    Update debpackage.sh

--- debpackage.sh
+++ debpackage.sh
@@ -6,11 +6,10 @@ https://github.com/balena-io/etcher/releases/download/v2.1.0/balena-etcher_2.1.0
 https://dldir1v6.qq.com/weixin/Universal/Linux/WeChatLinux_x86_64.deb   
 https://download1.rstudio.org/electron/jammy/amd64/rstudio-2024.12.1-563-amd64.deb    
 https://dl.hostbuf.com/finalshell3/finalshell_linux_x64.deb   
-https://vscode.download.prss.microsoft.com/dbazure/download/stable/6609ac3d66f4eade5cf376d1cb76f13985724bcb/code_1.98.0-1741124782_amd64.deb
 '       >      download.txt
 xargs   --max-args 1        --max-procs 10   --arg-file   download.txt        \
 curl    --socks5-hostname   127.0.0.1:8000   --location   --continue-at -     --remote-name
-DEBIAN_FRONTEND=noninteractive   apt   -y   install   ./v2rayN-linux-64.deb   ./WeChatLinux_x86_64.deb   ./balena-etcher_2.1.0_amd64.deb   ./rstudio-2024.12.1-563-amd64.deb     ./finalshell_linux_x64.deb   ./code_1.98.0-1741124782_amd64.deb
+DEBIAN_FRONTEND=noninteractive   apt   -y   install   ./v2rayN-linux-64.deb   ./WeChatLinux_x86_64.deb   ./balena-etcher_2.1.0_amd64.deb   ./rstudio-2024.12.1-563-amd64.deb     ./finalshell_linux_x64.deb
 
 
 
