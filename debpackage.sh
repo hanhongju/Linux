622b2818be308fe40b66a983182b5114c60e14ca
hanhongju <63781648+hanhongju@users.noreply.github.com>
2025-03-18 10:38:45 +0800

    Update debpackage.sh

--- debpackage.sh
+++ debpackage.sh
@@ -7,7 +7,7 @@ https://dldir1v6.qq.com/weixin/Universal/Linux/WeChatLinux_x86_64.deb
 https://download1.rstudio.org/electron/jammy/amd64/rstudio-2024.12.1-563-amd64.deb
 https://github.com/Eugeny/tabby/releases/download/v1.0.223/tabby-1.0.223-linux-x64.deb
 '       >      download.txt
-xargs   --max-args 1        --max-procs 10   --arg-file   download.txt        \
+xargs   --max-args 1        --max-procs 10   --arg-file download.txt        \
 curl    --remote-name       --location       --continue-at -       --socks5-hostname   127.0.0.1:8000
 DEBIAN_FRONTEND=noninteractive   apt   -y   install   \
         ./v2rayN-linux-64.deb   ./WeChatLinux_x86_64.deb   ./balena-etcher_2.1.0_amd64.deb   ./rstudio-2024.12.1-563-amd64.deb    ./tabby-1.0.223-linux-x64.deb
