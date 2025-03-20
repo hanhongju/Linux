6ce74b4d616b1f86411f16c4f397476d6ef8473f
hanhongju <63781648+hanhongju@users.noreply.github.com>
2025-03-20 17:22:07 +0800

    Update Nw_Coput.sh

--- Nw_Coput.sh
+++ Nw_Coput.sh
@@ -4,15 +4,16 @@ Suites: noble noble-updates noble-security
 Components: main restricted universe multiverse
 Signed-By: /usr/share/keyrings/ubuntu-archive-keyring.gpg
 '       >     /etc/apt/sources.list.d/ubuntu.sources
+# Comprehensive R Archive Network mirror sources
+echo    'deb https://mirrors.ustc.edu.cn/CRAN/bin/linux/ubuntu noble-cran40/'                     >    /etc/apt/sources.list.d/cran40.list
+echo    'deb [arch=amd64,arm64,armhf] https://packages.microsoft.com/repos/code/ stable main'     >    /etc/apt/sources.list.d/vscode.list
 echo    '
 https://mirrors.ustc.edu.cn/CRAN/bin/linux/ubuntu/marutter_pubkey.asc
 https://packages.microsoft.com/keys/microsoft.asc
 '       >      trustedkey.txt
 xargs   --max-args 1        --max-procs 10        --arg-file trustedkey.txt      \
 curl    --location          --continue-at -       --socks5-hostname 127.0.0.1:5000      --output-dir /etc/apt/trusted.gpg.d/       --remote-name
-# Comprehensive R Archive Network mirror sources
-echo    'deb https://mirrors.ustc.edu.cn/CRAN/bin/linux/ubuntu noble-cran40/'                     >    /etc/apt/sources.list.d/cran40.list
-echo    'deb [arch=amd64,arm64,armhf] https://packages.microsoft.com/repos/code/ stable main'     >    /etc/apt/sources.list.d/vscode.list
+
 
 
 
