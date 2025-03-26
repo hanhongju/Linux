5a72b7fec1c4c7c0ed3d25bff03e39b91100f7aa
hanhongju <63781648+hanhongju@users.noreply.github.com>
2025-03-26 21:22:43 +0800

    Update Arch_Nw_Coput.sh

--- Arch_Nw_Coput.sh
+++ Arch_Nw_Coput.sh
@@ -1,2 +1,16 @@
-pacman -Syy
-pacman -S xorg
+echo    'Server = https://mirrors.nju.edu.cn/manjaro/stable/$repo/$arch'        >    /etc/pacman.d/mirrorlist
+pacman  --sync -yy
+pacman  --sync --noconfirm curl
+
+
+
+
+
+
+
+
+
+
+
+
+# 镜像源列表 https://archlinux.org/download/
