d0db2a4ec2e04e2bb7338034aa8b6750cf08bace
hanhongju <63781648+hanhongju@users.noreply.github.com>
2025-03-27 22:51:47 +0800

    Update Nw_Coput.sh

--- Nw_Coput.sh
+++ Nw_Coput.sh
@@ -19,15 +19,49 @@ echo    'deb [arch=amd64,arm64,armhf] https://packages.microsoft.com/repos/code/
 
 
 apt     update
-dpkg    -a     --configure
-DEBIAN_FRONTEND=noninteractive   apt     -y     install   \
+dpkg    -a       --configure
+DEBIAN_FRONTEND=noninteractive   apt   -y   full-upgrade   &&   apt   -y   autoremove
+DEBIAN_FRONTEND=noninteractive   apt   -y   install    \ 
         wget curl brasero rhythmbox smplayer net-tools libreoffice libreoffice-l10n-zh-cn libreoffice-help-zh-cn \
         hashdeep p7zip-full firefox gparted usb-creator-gtk samba qbittorrent cifs-utils steam thunderbird code  \
         handbrake libcurl4-openssl-dev software-properties-common dirmngr r-base libmagick++-dev cmake calibre   \
-        krita krita-l10n gimp
-apt     -y     autoremove
-ubuntu-drivers autoinstall
-netstat        -plnt
+        krita krita-l10n gimp trojan
+ubuntu-drivers   autoinstall
+netstat          -plnt
+
+
+
+
+site=alihk.hanhongju.com
+echo '
+{"run_type"    : "client"
+,"local_addr"  : "127.0.0.1"
+,"local_port"  : 4000
+,"remote_addr" : "www.example.com"
+,"remote_port" : 443
+,"password"    : ["fengkuang"]
+,"ssl"         : {"sni"  : "www.example.com"
+                 ,"alpn" : ["http/1.1"]
+                 }
+}
+'           >                                             /etc/trojan/config.json
+sed         -i        "s/www.example.com/$site/g"         /etc/trojan/config.json
+systemctl   enable    trojan
+systemctl   restart   trojan
+echo   '
+https://github.com/2dust/v2rayN/releases/download/7.9.3/v2rayN-linux-64.deb
+https://github.com/balena-io/etcher/releases/download/v2.1.0/balena-etcher_2.1.0_amd64.deb
+https://dldir1v6.qq.com/weixin/Universal/Linux/WeChatLinux_x86_64.deb
+https://download1.rstudio.org/electron/jammy/amd64/rstudio-2024.12.1-563-amd64.deb
+https://github.com/Eugeny/tabby/releases/download/v1.0.223/tabby-1.0.223-linux-x64.deb
+https://www.softmaker.net/down/softmaker-freeoffice-2024_1224-01_amd64.deb
+'       >      download.txt
+xargs   --max-args 1        --max-procs 10        --arg-file download.txt        \
+curl    --location          --continue-at -       --socks5-hostname 127.0.0.1:4000      --remote-name
+DEBIAN_FRONTEND=noninteractive   apt   -y   install   \
+        ./v2rayN-linux-64.deb   ./WeChatLinux_x86_64.deb   ./balena-etcher_2.1.0_amd64.deb  \
+        ./rstudio-2024.12.1-563-amd64.deb   ./tabby-1.0.223-linux-x64.deb                   \
+        ./softmaker-freeoffice-2024_1224-01_amd64.deb
 
 
 
