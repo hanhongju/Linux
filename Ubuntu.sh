774228bb64678dc4d373890dbc72d0496c106d26
hanhongju <63781648+hanhongju@users.noreply.github.com>
2025-03-31 14:16:29 +0800

    Update and rename Linuxmint.sh to Ubuntu.sh

--- Ubuntu.sh
+++ Ubuntu.sh
@@ -1,5 +1,10 @@
-echo   'deb https://mirror.nju.edu.cn/linuxmint/ wilma main upstream import backport'   >   /etc/apt/sources.list.d/official-package-repositories.list
-apt     update   &&   apt   install   -y   curl
+echo   'Types: deb
+URIs: http://mirrors.nju.edu.cn/ubuntu/
+Suites: noble noble-updates noble-security
+Components: main restricted universe multiverse
+Signed-By: /usr/share/keyrings/ubuntu-archive-keyring.gpg
+'       >     /etc/apt/sources.list.d/nju.sources
+apt     update   &&   apt   -y   install   curl
 echo    '
 https://mirrors.nju.edu.cn/CRAN/bin/linux/ubuntu/marutter_pubkey.asc
 https://packages.microsoft.com/keys/microsoft.asc
@@ -9,14 +14,15 @@ curl    --location          --continue-at -       --output-dir /etc/apt/trusted.
 # Comprehensive R Archive Network mirror sources
 echo    'deb https://mirrors.nju.edu.cn/CRAN/bin/linux/ubuntu noble-cran40/'                      >    /etc/apt/sources.list.d/cran40.list
 echo    'deb [arch=amd64,arm64,armhf] https://packages.microsoft.com/repos/code/ stable main'     >    /etc/apt/sources.list.d/vscode.list
-rm      -rf      /etc/apt/preferences.d/*
-apt     update   &&   DEBIAN_FRONTEND=noninteractive   apt   install   -y   \
+apt     update   &&   DEBIAN_FRONTEND=noninteractive   apt   -y   install   \
         wget curl brasero rhythmbox smplayer net-tools libreoffice libreoffice-l10n-zh-cn libreoffice-help-zh-cn \
-        hashdeep p7zip-full gparted usb-creator-gtk samba qbittorrent cifs-utils steam code firefox thunderbird  \
+        hashdeep p7zip-full firefox gparted usb-creator-gtk samba qbittorrent cifs-utils steam thunderbird code  \
         handbrake libcurl4-openssl-dev software-properties-common dirmngr r-base libmagick++-dev cmake calibre   \
         krita krita-l10n gimp trojan
 dpkg        -a   --configure
 ubuntu-drivers   autoinstall
+
+
 
 
 
