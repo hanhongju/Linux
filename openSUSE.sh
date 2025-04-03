a3ece13b17f194f3837d083b2c1c0b04f7f72e81
hanhongju <63781648+hanhongju@users.noreply.github.com>
2025-04-03 14:41:08 +0800

    Update openSUSE.sh

--- openSUSE.sh
+++ openSUSE.sh
@@ -15,41 +15,8 @@ gpgcheck=1
 zypper  refresh
 zypper  --non-interactive install brasero rhythmbox smplayer libreoffice hashdeep gparted qbittorrent cmake krita gimp trojan R-base  \
                                  gcc gcc-c++ gcc-fortran harfbuzz-devel fribidi-devel freetype-devel fontconfig-devel                \
-                                 libxml2-devel libcurl-devel libjpeg-devel libpng-devel libtiff-devel net-tools
-
-
-
-
-site=alihk.hanhongju.com
-echo '
-{"run_type"    : "client"
-,"local_addr"  : "127.0.0.1"
-,"local_port"  : 4000
-,"remote_addr" : "www.example.com"
-,"remote_port" : 443
-,"password"    : ["fengkuang"]
-,"ssl"         : {"sni"  : "www.example.com"
-                 ,"alpn" : ["http/1.1"]
-                 }
-}
-'           >                                             /etc/trojan/config.json
-sed         -i        "s/www.example.com/$site/g"         /etc/trojan/config.json
-systemctl   enable    trojan
-systemctl   restart   trojan
-zypper      --non-interactive   update
-echo   '
-https://vscode.download.prss.microsoft.com/dbazure/download/stable/ddc367ed5c8936efe395cffeec279b04ffd7db78/code-1.98.2-1741788968.el8.x86_64.rpm
-https://github.com/Eugeny/tabby/releases/download/v1.0.223/tabby-1.0.223-linux-x64.rpm
-https://dldir1v6.qq.com/weixin/Universal/Linux/WeChatLinux_x86_64.rpm
-https://www.softmaker.net/down/softmaker-freeoffice-2024-1224.x86_64.rpm
-https://download1.rstudio.org/electron/rhel9/x86_64/rstudio-2024.12.1-563-x86_64.rpm
-https://github.com/balena-io/etcher/releases/download/v2.1.0/balena-etcher-2.1.0-1.x86_64.rpm
-'       >      download.txt
-xargs   --max-args 1        --max-procs 10        --arg-file download.txt        \
-curl    --location          --continue-at -       --socks5-hostname 127.0.0.1:4000      --remote-name
-zypper  --non-interactive install  ./code-1.98.2-1741788968.el8.x86_64.rpm  ./tabby-1.0.223-linux-x64.rpm \
-                                  ./WeChatLinux_x86_64.rpm ./softmaker-freeoffice-2024-1224.x86_64.rpm   \
-                                  ./rstudio-2024.12.1-563-x86_64.rpm ./balena-etcher-2.1.0-1.x86_64.rpm
+                                 libxml2-devel libcurl-devel libjpeg-devel libpng-devel libtiff-devel net-tools fonts-noto-cjk
+zypper  --non-interactive update
 ss      --processes  --listening  --numeric  --tcp
 
 
