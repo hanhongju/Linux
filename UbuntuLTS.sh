d0ffffc8fc2a3284f838b02ec62d0528b11e5985
hanhongju <63781648+hanhongju@users.noreply.github.com>
2025-04-21 18:37:49 +0800

    Update UbuntuLTS.sh

--- UbuntuLTS.sh
+++ UbuntuLTS.sh
@@ -5,17 +5,19 @@ Suites: noble noble-updates noble-security noble-backports
 Components: main restricted universe multiverse
 Signed-By: /usr/share/keyrings/ubuntu-archive-keyring.gpg
 '       >     /etc/apt/sources.list.d/ubuntu.sources
-apt     update   &&   apt   -y   install   curl
-curl    --location  --continue-at -  --output-dir /etc/apt/trusted.gpg.d/  --remote-name https://packages.microsoft.com/keys/microsoft.asc
-curl    --location  --continue-at -  --output-dir /etc/apt/trusted.gpg.d/  --remote-name https://mirrors.nju.edu.cn/CRAN/bin/linux/ubuntu/marutter_pubkey.asc
-echo    'deb [arch=amd64,arm64,armhf] https://packages.microsoft.com/repos/code/      stable main'     >    /etc/apt/sources.list.d/vscode.list
+apt     update   &&   apt   -y   install   wget
+wget    -c    https://mirrors.nju.edu.cn/CRAN/bin/linux/ubuntu/marutter_pubkey.asc    -P   /etc/apt/trusted.gpg.d/
+wget    -c    https://www.virtualbox.org/download/oracle_vbox_2016.asc                -P   /etc/apt/trusted.gpg.d/
+wget    -c    https://packages.microsoft.com/keys/microsoft.asc                       -P   /etc/apt/trusted.gpg.d/
 # 官方源中的 r-base 版本较低，会造成错误
 echo    'deb https://mirrors.nju.edu.cn/CRAN/bin/linux/ubuntu noble-cran40/'                           >    /etc/apt/sources.list.d/cran40.list
+echo    'deb [arch=amd64] https://download.virtualbox.org/virtualbox/debian/ noble contrib'            >    /etc/apt/sources.list.d/virtualbox.list
+echo    'deb [arch=amd64,arm64,armhf] https://packages.microsoft.com/repos/code/ stable main'          >    /etc/apt/sources.list.d/vscode.list
 apt     update   &&   DEBIAN_FRONTEND=noninteractive   apt   -y   install   \
         wget curl brasero rhythmbox smplayer net-tools libreoffice libreoffice-l10n-zh-cn libreoffice-help-zh-cn \
         hashdeep p7zip-full firefox gparted usb-creator-gtk samba qbittorrent cifs-utils thunderbird code  \
         handbrake libcurl4-openssl-dev software-properties-common dirmngr r-base libmagick++-dev cmake calibre   \
-        krita krita-l10n gimp trojan libharfbuzz-dev libfribidi-dev
+        krita krita-l10n gimp trojan libharfbuzz-dev libfribidi-dev virtualbox
 dpkg        -a   --configure
 ubuntu-drivers   autoinstall
 
@@ -48,7 +50,7 @@ https://github.com/Eugeny/tabby/releases/download/v1.0.223/tabby-1.0.223-linux-x
 https://www.softmaker.net/down/softmaker-freeoffice-2024_1224-01_amd64.deb
 '       >      download.txt
 xargs   --max-args 1        --max-procs 10        --arg-file download.txt        \
-curl    --location          --continue-at -       --socks5-hostname 127.0.0.1:4000      --remote-name
+curl    --location          --continue-at -       --socks5-hostname 127.0.0.1:4000      --output-dir   ./     --remote-name
 DEBIAN_FRONTEND=noninteractive   apt   -y   install   \
         ./v2rayN-linux-64.deb   ./WeChatLinux_x86_64.deb   ./balena-etcher_2.1.0_amd64.deb  \
         ./rstudio-2024.12.1-563-amd64.deb   ./tabby-1.0.223-linux-x64.deb                   \
